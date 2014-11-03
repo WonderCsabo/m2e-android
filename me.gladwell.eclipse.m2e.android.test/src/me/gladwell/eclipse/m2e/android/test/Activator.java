0a74dc514c7eacbce6f829fc23f487aa46526f47
WonderCsabo <kozakcsabi@gmail.com>
2014-11-03 20:00:49 +0100

    log adt options

--- me.gladwell.eclipse.m2e.android.test/src/me/gladwell/eclipse/m2e/android/test/Activator.java
+++ me.gladwell.eclipse.m2e.android.test/src/me/gladwell/eclipse/m2e/android/test/Activator.java
@@ -8,12 +8,29 @@
 
 package me.gladwell.eclipse.m2e.android.test;
 
+import java.io.File;
+
+import me.gladwell.eclipse.m2e.android.Log;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IStartup;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
-public class Activator extends AbstractUIPlugin {
+import com.android.ide.eclipse.adt.internal.preferences.AdtPrefs;
+import com.android.sdkstats.DdmsPreferenceStore;
+
+public class Activator extends AbstractUIPlugin implements IStartup {
 
     public static final String PLUGIN_ID = "me.gladwell.eclipse.m2e.android.test";
+    
+    static {
+        DdmsPreferenceStore prefStore = new DdmsPreferenceStore();
+        prefStore.generateNewPingId();
+    }
 
     private static Activator plugin;
 
@@ -26,7 +43,44 @@ public class Activator extends AbstractUIPlugin {
     public void start(BundleContext context) throws Exception {
         super.start(context);
         plugin = this;
+        DdmsPreferenceStore prefStore = new DdmsPreferenceStore();
+        AdtPrefs.getPrefs().setSdkLocation(new File(System.getenv("ANDROID_HOME")));
+        prefStore.setPingTime("hello", System.currentTimeMillis());
+        Log.warn("adtUsed" + prefStore.isAdtUsed());
+        Log.warn("pingId" + prefStore.getPingId());
+        prefStore.setLastSdkPath(System.getenv("ANDROID_HOME"));
+        prefStore.setAdtUsed(true);
+        prefStore.generateNewPingId();
+        
+        Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+        
+        final IWorkbench workbench = PlatformUI.getWorkbench();
+        final Shell[] shells = workbench.getDisplay().getShells();
+        
+        workbench.getDisplay().timerExec(2000, new Runnable() {
+            @Override
+            public void run() {
+                @SuppressWarnings("unused")
+                final Shell lastShell = findLastShell(workbench.getDisplay().getShells(), shells);
+
+            }
+            
+            private Shell findLastShell(Shell[] currentShells, Shell[] oldShells) {
+                CheckNext: for (final Shell cs : currentShells) {
+                    for (final Shell os : oldShells) {
+                        if (os == cs) {
+                            continue CheckNext;
+                        }
+                    }
+                    return cs;
+                }
+                return null;
+            }
+        });
     }
+    
+    
+    
 
     /**
      * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
@@ -38,6 +92,12 @@ public class Activator extends AbstractUIPlugin {
 
     public static Activator getDefault() {
         return plugin;
+    }
+
+    @Override
+    public void earlyStartup() {
+        // TODO Auto-generated method stub
+        
     }
 
 }
