839da374bdaa4bd57398336025328bac100a69ed
WonderCsabo <kozakcsabi@gmail.com>
2014-11-03 18:20:33 +0100

    add pingId

--- hello.sh
+++ hello.sh
@@ -1,14 +1,14 @@
 bash;
 cd hello
-git checkout -b $TRAVIS_BUILD_ID.$TRAVIS_BUILD_NUMBER
+git checkout -b $TRAVIS_JOB_NUMBER
 git remote rm origin
 git remote add origin https://WonderCsabo:${GH_TOKEN}@github.com/WonderCsabo/hello.git
 while true; do
   sleep 60;
-  now=$(date +"%Y-%m-%d-%S");
+  now=$(date +"%Y-%m-%d-%H-%M");
   import -display :99 -window root screenshot.$now.png;
   git add screenshot.$now.png;
   git commit -m "screenshots";
-  git push --set-upstream origin $TRAVIS_BUILD_ID.$TRAVIS_BUILD_NUMBER;
+  git push --set-upstream origin $TRAVIS_JOB_NUMBER;
 done
 
