65915b1abb426628387f50d673924908c59e4ad3
WonderCsabo <kozakcsabi@gmail.com>
2014-11-03 13:29:38 +0100

    upload shots after failure

--- hello.sh
+++ hello.sh
@@ -1,2 +1,2 @@
-while true; do now=$(date +"%Y-%m-%d-%S"); import -display :99 -window root screenshot.$now.png; sleep 60; done
+while true; do now=$(date +"%Y-%m-%d-%S"); import -display :99 -window root hello/screenshot.$now.png; sleep 60; done
 
