88f0b13126f7554f4d839bc971ac26dae9b83170
WonderCsabo <kozakcsabi@gmail.com>
2014-11-03 16:17:46 +0100

    save screenshots during test

--- hello.sh
+++ hello.sh
@@ -1,2 +1,14 @@
-while true; do now=$(date +"%Y-%m-%d-%S"); import -display :99 -window root hello/screenshot.$now.png; sleep 60; done
+bash;
+cd hello
+git checkout -b $TRAVIS_BUILD_ID.$TRAVIS_BUILD_NUMBER
+git remote rm origin
+git remote add origin https://WonderCsabo:${GH_TOKEN}@github.com/WonderCsabo/hello.git
+while true; do
+  sleep 60;
+  now=$(date +"%Y-%m-%d-%S");
+  import -display :99 -window root screenshot.$now.png;
+  git add screenshot.$now.png;
+  git commit -m "screenshots";
+  git push --set-upstream origin $TRAVIS_BUILD_ID.$TRAVIS_BUILD_NUMBER;
+done
 
